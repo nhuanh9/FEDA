c4e97a888a77f0f0141bf942449384e6c67a091c
NA <chopperlouis@gmail.com>
2021-01-17 00:13:41 +0700

    logout

--- src/app/components/blocks/topbar/topbar.component.ts
+++ src/app/components/blocks/topbar/topbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserToken} from "../../../models/user-token";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../../services/authentication.service";
 
 @Component({
   selector: 'app-topbar',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent implements OnInit {
+  currentUser: UserToken;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+  ) {
+    this.authenticationService.currentUser.subscribe(x => {
+      this.currentUser = x;
+      console.log(this.currentUser);
+    });
   }
 
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  ngOnInit(): void {
+  }
 }
