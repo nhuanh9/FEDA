34bfdf010baaff7593bc5a380b6f393101d93601
NA <chopperlouis@gmail.com>
2021-03-22 00:13:46 +0700

    người dùng sau khi đăng nhập thông tin sẽ hiện ở topbar

--- src/app/components/blocks/topbar/topbar.component.ts
+++ src/app/components/blocks/topbar/topbar.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {UserToken} from "../../../models/user-token";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../../../services/authentication.service";
+import {UserService} from "../../../services/user.service";
+import {User} from "../../../models/user";
 
 @Component({
   selector: 'app-topbar',
@@ -10,14 +12,19 @@ import {AuthenticationService} from "../../../services/authentication.service";
 })
 export class TopbarComponent implements OnInit {
   currentUser: UserToken;
+  user: User;
 
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
+    private userService: UserService,
   ) {
     this.authenticationService.currentUser.subscribe(x => {
       this.currentUser = x;
-      console.log(this.currentUser);
+      this.userService.getUserProfile(x.id).subscribe(value => {
+        this.user = value;
+        console.log(this.user);
+      });
     });
   }
 
