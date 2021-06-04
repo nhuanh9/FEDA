e431f6dc32123511d103b7b7088dddef77abc473
NA <chopperlouis@gmail.com>
2021-06-05 01:11:09 +0700

    done add user from register 1/2

--- src/app/components/pages/user/register/register.component.ts
+++ src/app/components/pages/user/register/register.component.ts
@@ -4,6 +4,7 @@ import {UserService} from "../../../../services/user.service";
 import {Router} from "@angular/router";
 import {User} from "../../../../models/user";
 import {ItutcService} from "../../../../services/itutc.service";
+import {CometChat} from "@cometchat-pro/chat/CometChat";
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ import {ItutcService} from "../../../../services/itutc.service";
 export class RegisterComponent implements OnInit {
   itutcs: any[];
   msv: any;
+  authKey = "f51cb914320b1239d05b6d32ea22727de7e9a558";
   registerForm: FormGroup = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
     password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
@@ -28,28 +30,37 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.setCommetChat();
   }
 
   register() {
     const user = this.setNewUser();
-    this.itutcService.getAll().subscribe(res=>{
-      this.itutcs = res;
-      for (let i=0; i<this.itutcs.length; i++) {
-        if (this.itutcs[i].msv === user.status){
-          user.status = '2';
-        }
+    // this.itutcService.getAll().subscribe(res=>{
+    //   this.itutcs = res;
+    //   for (let i=0; i<this.itutcs.length; i++) {
+    //     if (this.itutcs[i].msv === user.status){
+    //       user.status = '2';
+    //     }
+    //   }
+    //   this.userService.register(user).subscribe(() => {
+    //     alert('Đăng ký thành công');
+    //     this.registerForm.reset();
+    //     this.router.navigate(['/login']);
+    //   }, err => {
+    //     alert("Tài khoản đã được đăng ký!");
+    //   });
+    //   console.log(user);
+    // })
+    const userc = new CometChat.User(user.username);
+    userc.setName(user.name);
+    console.log(user)
+    CometChat.createUser(userc, this.authKey).then(
+      user => {
+        console.log("user created", user);
+      },error => {
+        console.log("error", error);
       }
-      this.userService.register(user).subscribe(() => {
-        alert('Đăng ký thành công');
-        this.registerForm.reset();
-        this.router.navigate(['/login']);
-      }, err => {
-        alert("Tài khoản đã được đăng ký!");
-      });
-      console.log(user);
-    })
-
+    )
 
   }
 
@@ -72,5 +83,26 @@ export class RegisterComponent implements OnInit {
     } else {
       document.getElementById('truong').style.display = 'none';
     }
+
+
+  }
+  setCommetChat() {
+    const appID = "343009d4d365f12";
+    const region = "EU";
+    const appSetting = new CometChat.AppSettingsBuilder()
+      .subscribePresenceForAllUsers()
+      .setRegion(region)
+      .build();
+    console.log(appSetting)
+    CometChat.init(appID, appSetting).then(
+      () => {
+        console.log("Initialization completed successfully");
+        // You can now call login function.
+      },
+      (error) => {
+        console.log("Initialization failed with error:", error);
+        // Check the reason for error and take appropriate action.
+      }
+    );
   }
 }
